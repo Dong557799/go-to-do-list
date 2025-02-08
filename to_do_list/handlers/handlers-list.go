4161b5fd2fe83d1c608d4f97a6f0a6ebec87f9a4
Dong557799 <3031267623@qq.com>
2025-02-08 22:26:26 +0800

    Update handlers-list.go

--- to_do_list/handlers/handlers-list.go
+++ to_do_list/handlers/handlers-list.go
@@ -1,7 +1,7 @@
 package handlers
 
 import (
-	"net/http"
+	"net/http"//提供了 HTTP 客户端和服务器的实现
 	"strconv"
 	"to_do_list/models"
 
@@ -16,24 +16,31 @@ func CreateList(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
+	//为新创建的清单分配一个唯一的 ID
 	list.ID = models.ListID
 	models.Lists[models.ListID] = list
+	//为下一个新创建的清单分配一个不同的 ID
 	models.ListID++
+	//返回HTTP 响应
 	c.JSON(http.StatusOK, list)
 }
 
 // GetLists 获取清单列表处理函数
 func GetLists(c *gin.Context) {
+	//声明models.List类型切片
 	var listArray []models.List
+	//遍历清单并将其添加到 listArray 切片
 	for _, list := range models.Lists {
 		listArray = append(listArray, list)
 	}
+	//返回HTTP响应
 	c.JSON(http.StatusOK, listArray)
 }
 
 // UpdateList 更新清单处理函数
 func UpdateList(c *gin.Context) {
 	// 修改为 :listID
+	//获取名为 listID 的参数值
 	listIDStr := c.Param("listID")
 	listID, err := strconv.Atoi(listIDStr)
 	if err != nil {
@@ -44,12 +51,15 @@ func UpdateList(c *gin.Context) {
 		c.JSON(http.StatusNotFound, gin.H{"error": "List not found"})
 		return
 	}
+	
+	//存储从请求体中解析出来的更新后的清单数据
 	var updatedList models.List
 	if err := c.ShouldBindJSON(&updatedList); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 	updatedList.ID = listID
+	//覆盖原来的清单
 	models.Lists[listID] = updatedList
 	c.JSON(http.StatusOK, updatedList)
 }
